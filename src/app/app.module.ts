3116d6ab0cb5f4294291b3790557af2589d2444e
Michael Moffet <michael.moffet@broadbean.com>
2017-01-30 13:22:09 -0800

    feat(game list): add scroll to top
    Add fixed button that scrolls back to top of master list without triggering change detection by
    running outside of the Angular zone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import {GameSummaryPage} from '../pages/game-summary/game-summary';
 import {DealsFilterPage} from '../pages/deals-filter/deals-filter';
 import {FilterService} from '../providers/filter-service';
 import {FilterPipe} from '../pipes/filter-pipe';
+import {ScrollToTopPage} from '../pages/scroll-to-top/scroll-to-top';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     GameSummaryPage,
+    ScrollToTopPage,
     DealsFilterPage,
     FilterPipe
   ],
@@ -24,6 +26,7 @@ import {FilterPipe} from '../pipes/filter-pipe';
     MyApp,
     HomePage,
     GameSummaryPage,
+    ScrollToTopPage,
     DealsFilterPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, DealsData, FilterService]
---
+++ src/pages/scroll-to-top/scroll-to-top.ts
@@ -0,0 +1,47 @@
+import {Component, ChangeDetectionStrategy, ViewChild, ElementRef, NgZone, Input} from '@angular/core';
+import {Subscription, Observable} from 'rxjs';
+
+@Component({
+  selector: 'page-scroll-to-top',
+  templateUrl: 'scroll-to-top.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ScrollToTopPage {
+
+  private _subs:Subscription[] = [];
+  @Input()
+  gameList;
+
+  // reference to template variable
+  @ViewChild('scrollButton') scrollButton:ElementRef;
+
+  constructor(private _zone:NgZone){
+
+  }
+
+  ngOnInit() {
+    this.manuallyBindToViewEvents()
+  }
+
+  manuallyBindToViewEvents() {
+    this.subscribeToMyButton()
+  }
+
+  subscribeToMyButton() {
+    let sub:Subscription;
+
+    this._zone.runOutsideAngular(() => {
+      sub = Observable.fromEvent(this.scrollButton.nativeElement, 'click').subscribe(e => {
+        this.gameList.scrollToTop();
+      })
+    });
+
+    this._subs.push(sub);
+  }
+
+  ngOnDestroy() {
+    // clean up subscriptions
+    this._subs.forEach(sub => sub.unsubscribe());
+  }
+
+}
